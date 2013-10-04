257c7fe103aa1008bf729aa70b4f758485e0b989
Mario Jr <mariogasparoni@gmail.com>
2013-10-04 01:53:02 -0300

    restart with postgres

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LojavirtualInf::Application.config.secret_token = 'f13a482f4b9d91b77fbe9554d579b5ebf67178c85bd61aa1aa65399f3651a74b5251a53ffa36a56f2a3e7a226ca0f088ddc52b872b7c4b1c3028939f855c2e6c'
