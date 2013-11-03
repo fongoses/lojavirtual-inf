4ec7596891c951072598affffd3f0b41d9ddaedd
Luiz Souza <fongoses@gmail.com>
2013-11-03 21:33:45 -0200

    Ajustes da base de dados e criação dos scripts de inicialização de uf e país

---
+++ db/inicializacao/uf.sql
@@ -0,0 +1,31 @@
+/*
+-- Query: 
+-- Date: 2013-11-03 21:25
+*/
+INSERT INTO `uf` (`iduf`,`nome`) VALUES ('AC','Acre');
+INSERT INTO `uf` (`iduf`,`nome`) VALUES ('AL','Alagoas');
+INSERT INTO `uf` (`iduf`,`nome`) VALUES ('AP','Amapá');
+INSERT INTO `uf` (`iduf`,`nome`) VALUES ('AM','Amazonas');
+INSERT INTO `uf` (`iduf`,`nome`) VALUES ('BA','Bahia');
+INSERT INTO `uf` (`iduf`,`nome`) VALUES ('CE','Ceará');
+INSERT INTO `uf` (`iduf`,`nome`) VALUES ('DF','Distrito Federal');
+INSERT INTO `uf` (`iduf`,`nome`) VALUES ('ES','Espírito Santo');
+INSERT INTO `uf` (`iduf`,`nome`) VALUES ('GO','Goiás');
+INSERT INTO `uf` (`iduf`,`nome`) VALUES ('MA','Maranhão');
+INSERT INTO `uf` (`iduf`,`nome`) VALUES ('MT','Mato Grosso');
+INSERT INTO `uf` (`iduf`,`nome`) VALUES ('MS','Mato Grosso do Sul');
+INSERT INTO `uf` (`iduf`,`nome`) VALUES ('MG','Minas Gerais');
+INSERT INTO `uf` (`iduf`,`nome`) VALUES ('PA','Pará');
+INSERT INTO `uf` (`iduf`,`nome`) VALUES ('PB','Paraíba');
+INSERT INTO `uf` (`iduf`,`nome`) VALUES ('PR','Paraná');
+INSERT INTO `uf` (`iduf`,`nome`) VALUES ('PE','Pernambuco');
+INSERT INTO `uf` (`iduf`,`nome`) VALUES ('PI','Piauí');
+INSERT INTO `uf` (`iduf`,`nome`) VALUES ('RJ','Rio de Janeiro');
+INSERT INTO `uf` (`iduf`,`nome`) VALUES ('RN','Rio Grande do Norte');
+INSERT INTO `uf` (`iduf`,`nome`) VALUES ('RS','Rio Grande do Sul');
+INSERT INTO `uf` (`iduf`,`nome`) VALUES ('RO','Rondônia');
+INSERT INTO `uf` (`iduf`,`nome`) VALUES ('RR','Roraima');
+INSERT INTO `uf` (`iduf`,`nome`) VALUES ('SC','Santa Catarina');
+INSERT INTO `uf` (`iduf`,`nome`) VALUES ('SP','São Paulo');
+INSERT INTO `uf` (`iduf`,`nome`) VALUES ('SE','Sergipe');
+INSERT INTO `uf` (`iduf`,`nome`) VALUES ('TO','Tocantins');
