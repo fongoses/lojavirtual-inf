4cfe58f84e675b581e171fb06d5d5e90c90b7fa9
Mario Jr <mariogasparoni@gmail.com>
2013-11-24 16:07:36 -0200

    Crud de produtos em principio ok.

---
+++ controller/produto/deleta_produto.php
@@ -0,0 +1,53 @@
+<?php
+/*
+ * Created on 19/01/2012
+ *
+ * Cadastra novo usuário
+ */ 
+ 		 	
+ 	if(!defined("__MVCPATHS__")) {
+	define("__MVCPATHS__", "true");
+	
+		$MAINPATH=$_SERVER['DOCUMENT_ROOT'].'lojavirtual';
+	 	$CONTROLLERPATH=$MAINPATH.'/controller';
+	 	$MODELPATH = $MAINPATH.'/model';
+	
+	}
+ 	
+ 	/* --Includes/Imports---------------------------------------------*/
+ 	include($CONTROLLERPATH.'/session/restrict/sessionControl.php');
+ 	include($CONTROLLERPATH.'/errors/errors.php');
+ 	include($MODELPATH.'/produto/produto.php');
+ 	 	
+ 	header('Content-Type: text/html; charset=UTF-8');
+ 	
+  	//echo '<script type="text/javascript"> alert("aaa") </script>'
+ 	/* --Methods-----------------------------------------------------*/
+	 	
+	$idProduto = intval(mysql_real_escape_string($_POST['idproduto'])); //obter essa informcacao da sessao
+
+ 	$produto = new produto();
+
+ 	$produto->setIdProduto($idProduto); 
+ 	
+ 	//if($produto->belongsToUser($_SESSION['user']->getName()))
+ 		$resultado = $produto->delete();
+ 	
+ 	 	
+	if($resultado == ERRO__MYSQL__DADOSEMBRANCO){		 			
+ 		echo '<script type="text/javascript"> alert("Produto inexistente");location.href="../../view/produto/visualizacao/"</script>;';
+ 		return;
+ 	}
+ 	
+ 	if($resultado == ERRO__MYSQL__FALHACONEXAO){
+ 		echo '<script type="text/javascript"> alert("Falha na conexão");location.href="../../view/produto/visualizacao/"</script>;';
+ 		return;
+ 	} 	
+ 	
+ 	if ($resultado > 0) {
+		echo '<script type="text/javascript"> alert("Produto apagado com sucesso!");location.href="../../view/produto/visualizacao/"</script>;';
+		return;
+	}
+ 	
+?>
+
